a2d5e63d34d56a5e16d4efb4d07010d63d004684
KaueMagid <magidsalem96@gmail.com>
2021-06-21 21:01:11 -0300

    Create Enum and Entities

---
+++ Entities/Serie.cs
@@ -0,0 +1,44 @@
+﻿
+using ConsoleListSeries.Enums;
+using System;
+
+namespace ConsoleListSeries.Entities
+{
+    public class Serie: BaseEntity
+    {
+        private Genre Genre { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private int Year { get; set; }
+
+        public Serie(int id, Genre genre,string title, string description, int year)
+        {
+            this.Id = id;
+            this.Genre = genre;
+            this.Title = title;
+            this.Description = description;
+            this.Year = year;
+        }
+
+        public override string ToString()
+        {
+            string aws = "Genre: " + this.Genre + Environment.NewLine;
+            aws += "Title: " + this.Title + Environment.NewLine;
+            aws += "Description: " + this.Description + Environment.NewLine;
+            aws += "Year: " + this.Year + Environment.NewLine;
+            
+            return aws;
+        }
+        
+        public string GetTitle()
+        {
+            return this.Title;
+        }
+
+        public int GetId()
+        {
+            return this.Id;
+        }
+
+    }
+}
---
+++ Enums/Genre.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleListSeries.Enums
+{
+    public enum Genre
+    {
+        Action = 1,
+        Adventure = 2,
+        Horror = 3,
+        Animation = 4,
+        Comedy = 5,
+        Classic = 6,
+        Drama = 7,
+        SciFi= 8,
+        Thriller = 9,
+        Romance = 10,
+        RomanticComedy = 11,
+        Documentary = 12,
+        West = 13
+    }
+}
